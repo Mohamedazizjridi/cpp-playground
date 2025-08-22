f3d4369be5e62c77589aeb968371501d28ae3dde
Mohamedazizjridi <153727546+Mohamedazizjridi@users.noreply.github.com>
2025-08-22 16:27:43 +0100

    Update swap.cpp

--- memory/Pointers & References/swap.cpp
+++ memory/Pointers & References/swap.cpp
@@ -1,16 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-void swap(int &a , int &b){
-  int *ptra=&a;
-  int *ptrb=&b;
-  int temp=a;
-  *ptra=*ptrb;
-  *ptrb=temp;
+void swap(int* a , int* b){
+
+  int temp= *a;
+  *a=*b;
+  *b=temp;
 }
 int  main()
 {
   int a=3;
   int b=5;
-  swap(a,b);
+  swap(&a,&b);
   cout<<a<<" "<<b<<endl;
 }
